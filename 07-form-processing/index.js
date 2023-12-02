752a248bbe3fb35163334b37cc4ea12499cf518f
Xu Jie Sng <sngxujie@gmail.com>
2023-12-02 07:56:03 +0000

    add frui pricing assignment

--- 07-form-processing/index.js
+++ 07-form-processing/index.js
@@ -25,21 +25,83 @@ app.get("/add-food", function (req, res){
     res.render("add-food")
 })
 
+app.get("/fruits", function (req, res){
+    res.render("fruits")
+})
+
+// fruits
+app.post("/fruits", function(req, res){
+ const fruits = req.body.items
+ let selectedFruits = []
+ if (Array.isArray(fruits)) {
+    selectedFruits = fruits
+ } else if (!fruits) {
+    selectedFruits = []
+ }else{ 
+    selectedFruits = [ fruits ]
+ }
+
+ let total = 0
+
+// map table
+const fruitPricings = {
+    "apple" : 3,
+    "orange" : 6,
+    "banana" : 4,
+    "durian" : 15
+}
+
+for (let item of selectedFruits) {
+    if (fruitPricings[item]) {
+        total += fruitPricings[item]
+    }
+}
+
+// for (let item of selectedFruits) {
+//  if (item == "durian") {
+//     total += 15;
+//  } if (item == "apple") {
+//     total += 3;
+//  }if (item == "orange") {
+//     total += 6;
+//  }if (item == "banana") {
+//     total += 4;
+//  }
+//  }
+
+ res.send("Total Cost = $" + total)
+})
+
 // We want to add a route for POST /add-food
 app.post("/add-food", function (req, res){
     // There is a shorter way of doing this form extraction
-    const {foodName,calories, meal, cuisine} = req.body;
+    const {foodName,calories, meal, cuisine, tags} = req.body;
     // check if radio button
     if (!meal) {
         res.send("No meal selected")
     return
     }
-    
+    let selectedTags = [];
+
+// if (Array.isArray(tags)) {
+//     selectedTags = tags;
+// } else if (!tags) {
+//     selectedTags = []
+// } else{
+//     selectedTags = [ tags ]
+// }
+
+//Another way of doing things
+if (tags) {
+    selectedTags = Array.isArray(tags) ? tags : [tags]
+}
+
     res.render("food-summary", {
         foodName,
         calories,
         meal,
-        cuisine
+        cuisine,
+        selectedTags
     })
 
     // const foodName = req.body.foodName
@@ -51,7 +113,11 @@ app.post("/add-food", function (req, res){
     // When the form is sumbitted it is sent to "req" or request
 // the name="foodName" field will be the key value pair
 
-    
+// for tags there can be one of three:
+// 
+
+
+
 })
 
 
