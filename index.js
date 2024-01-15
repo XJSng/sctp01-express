a9307b9bf2a19890eef63026c09621af1d77f240
Xu Jie Sng <sngxujie@gmail.com>
2024-01-15 07:27:39 +0000

    set up express and nodemon

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express')
+const app = express()
+
+// add routes here
+app.get("/", (req, res)=> {
+    res.send("<h1>Hello from Express</h1>")
+})
+
+
+// Run server here
+
+app.listen(7000, ()=>{
+    console.log("Server has started")
+})
