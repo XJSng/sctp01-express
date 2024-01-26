29be2237fbdd24034fc4498b8216ec684222dc93
Xu Jie Sng <sngxujie@gmail.com>
2024-01-26 01:50:23 +0000

    restore crud changes

--- 05-express-CRUD/index.js
+++ 05-express-CRUD/index.js
@@ -17,20 +17,74 @@ wax.on(hbs.handlebars)
 wax.setLayoutPath('./views/layouts')
 
 //ROUTES HERE
-const baseURL = "https://petstore.swagger.io/v2"
-app.get("/pets", async(req, res)=>{
-
-    let response = await axios.get(baseURL + "/pet/findByStatus",{
-        params: {
-            'status': 'available'
-        }
+async function main() {
+    const db = await connectToMongoDB()
+    
+    // VIEW EQUIPMENT DATA
+    app.get("/equipment", async(req, res)=>{
+           const equipments = await db.collection('equipment_list').find({}).toArray()
+    
+        res.render("all_equipment.hbs",{
+            "all_equipment": equipments
+        })
     })
-    res.render("all_pets.hbs",{
-        "all_pets": response.data
+    
+    // READ VOLUNTEER DATA
+    app.get("/volunteers", async(req, res)=>{
+        const db = await connectToMongoDB()
+        const volunteers = await db.collection('volunteers').find({}).toArray()
+            res.render("all_volunteers.hbs",{
+            "all_volunteers": volunteers
+        })
     })
-})
-
-}
+    
+    // READ LIVESTREAM SERVICE DATA
+    app.get("/livestream-service", async(req, res)=>{
+        const db = await connectToMongoDB()
+        const livestreamServices = await db.collection('livestream_service').find({}).toArray()
+            res.render("all_livestreams.hbs",{
+            "all_livestreams": livestreamServices
+        })
+    })
+    
+    // CREATE Equipement
+    app.get('/equipment/create',async(req,res)=>{
+        res.render('create_equipment.hbs')
+    })
+    
+    app.post("/equipment/create", async (req, res)=>{
+        let equipmentName  = req.body.equipmentName
+             dateOfPurchase = req.body.dateOfPurchase
+             equipmentType = req.body.equipmentType
+             modelNumber = req.body.modelNumber
+             generalRemarks = req.body.generalRemarks
+             serviceStatus = req.body.serviceStatus
+    
+     let data = {
+        "name": equipmentName,
+        "date-of-purchase": dateOfPurchase,
+        "equipment-type":equipmentType,
+        "model-number": modelNumber,
+        "general-remarks":generalRemarks,
+        "service": serviceStatus 
+     }
+        let result = await db.collection('equipment_list').insertOne(data)
+        res.status(201).json(result)
+    })
+    
+    //UPDATE ROUTE
+    app.get('/equipment/:equipment_id', async(req,res)=>{ 
+        const _id = req.params.equipment_id
+        const equipment = await db.collection('equipment_list').findOne({_id:_id})
+        console.log(equipment)
+        res.render('edit_equipment.hbs', {
+           "all_equipment":equipment
+        })
+    })
+    
+    
+    
+    }
 
 //ROUTES END
 
